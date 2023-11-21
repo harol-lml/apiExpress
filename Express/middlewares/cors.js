b21a0d2b7f1a161054d2fea9b694f84972d614a7
harol-lml <hapr0023@gmail.com>
2023-11-21 16:55:51 -0500

    fix validate

--- Express/middlewares/cors.js
+++ Express/middlewares/cors.js
@@ -2,13 +2,13 @@ import cors from 'cors'
 
 const ACCEPT_ORIGINS = [
     'http://localhost:8080',
-    'http://localhost:5500'
+    'http://127.0.0.1:5500'
 ]
 
 export const corsMiddleware = ({acceptedOrigins = ACCEPT_ORIGINS} = {}) => cors({
     origin: (origin, callback) => {
 
-        if(acceptedOrigins.includes(origin)) return callback(null, ture)
+        if(acceptedOrigins.includes(origin)) return callback(null, true)
 
         if(!origin) return callback(null, true)
 
