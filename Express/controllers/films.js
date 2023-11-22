e312c0477458d8135e7663189537c25acaefb232
harol-lml <hapr0023@gmail.com>
2023-11-22 11:25:17 -0500

    Se crea controlador

---
+++ Express/controllers/films.js
@@ -0,0 +1,49 @@
+import { FilmModel } from '../models/film.js'
+import { validateFilm, validateParcialFilm } from '../schemas/film.js'
+
+export class FilmController {
+    static async getAll (req, res) {
+        const { genre } = req.query
+        const films = await FilmModel.getAll({genre})
+
+        res.json(films)
+    }
+
+    static async getById (req, res) {
+        const {id} = req.params
+        const film = await FilmModel.getById({ id })
+
+        if (film) return res.json(film)
+        res.status(404).json({message: "film no found"})
+    }
+
+    static async create (req,res) {
+        const result = validateFilm(req.body)
+
+        if(result.error)
+            return res.status(400).json({ error: JSON.parse(result.error.message) })
+
+        const newFilm = await FilmModel.create({input: result.data})
+        res.status(201).json(newFilm)
+    }
+
+    static async update (req, res) {
+        const result = validateParcialFilm(req.body)
+
+        if(!result.success) return res.status(404).json({error: JSON.parse(result.error.message)})
+
+        const {id} = req.params
+        const updateFilm = await FilmModel.update({input: result.data, id})
+
+        if(!updateFilm) return res.status(404).json({message: 'Film not found'})
+        return res.json(updateFilm)
+    }
+
+    static async delete (req, res) {
+        const {id} = req.params
+        const film = await FilmModel.delete({ id })
+
+        if (!film) return res.status(404).json({message: 'Film not found'})
+        return res.json({message: 'Film deleted'})
+    }
+}
--- Express/reutes/films.js
+++ Express/reutes/films.js
@@ -1,52 +1,11 @@
 import { Router } from 'express'
-import { validateFilm, validateParcialFilm } from '../schemas/film.js'
-import { FilmModel } from '../models/film.js'
+import { FilmController } from '../controllers/films.js'
 
 export const filmRouter = Router()
 
-filmRouter.get('/', async (req, res) => {
-    const { genre } = req.query
-    const films = await FilmModel.getAll({genre})
+filmRouter.get('/', FilmController.getAll)
+filmRouter.post('/', FilmController.create)
 
-    res.json(films)
-})
-
-filmRouter.get('/:id', async (req, res) => {
-    const {id} = req.params
-
-    const film = await FilmModel.getById({ id })
-    if (film) return res.json(film)
-
-    res.status(404).json({message: "film no found"})
-})
-
-filmRouter.post('/', async (req,res) => {
-
-    const result = validateFilm(req.body)
-
-    if(result.error)
-        return res.status(400).json({ error: JSON.parse(result.error.message) })
-
-    const newFilm = await FilmModel.create({input: result.data})
-    res.status(201).json(newFilm)
-})
-
-filmRouter.patch('/:id', async (req, res) => {
-    const result = validateParcialFilm(req.body)
-
-    if(!result.success) return res.status(404).json({error: JSON.parse(result.error.message)})
-
-    const {id} = req.params
-    const updateFilm = await FilmModel.update({input: result.data, id})
-
-    if(!updateFilm) return res.status(404).json({message: 'Film not found'})
-    return res.json(updateFilm)
-})
-
-filmRouter.delete('/:id', async (req, res) => {
-    const {id} = req.params
-    const film = await FilmModel.delete({ id })
-
-    if (!film) return res.status(404).json({message: 'Film not found'})
-    return res.json({message: 'Film deleted'})
-})
+filmRouter.get('/:id', FilmController.getById)
+filmRouter.patch('/:id', FilmController.update)
+filmRouter.delete('/:id', FilmController.delete)
